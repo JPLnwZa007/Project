4de3092703a71b8421110dbc03965b62c2ea5d80
JPLnwZa007 <6531501185@lamduan.mfu.ac.th>
2025-05-10 18:24:37 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 10 18:17:36 2025
+
+@author: folkk
+"""
+
+import streamlit as st
+import pandas as pd
+import pickle
+
+# Load model and data
+model = pickle.load(open('kmeans_model.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+
+st.title("Customer Segmentation App")
+
+income = st.number_input("Income", min_value=0)
+kids = st.slider("Number of Kids", 0, 3)
+teens = st.slider("Number of Teens", 0, 3)
+recency = st.number_input("Recency", min_value=0)
+wines = st.number_input("Monthly Wine Spend")
+fruits = st.number_input("Monthly Fruit Spend")
+
+if st.button("Predict Segment"):
+    data = [[income, kids, teens, recency, wines, fruits]]
+    data_scaled = scaler.transform(data)
+    segment = model.predict(data_scaled)
+    st.success(f"Predicted Customer Segment: {segment[0]}")
